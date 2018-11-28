c9bf02f03e5bd48133893d86d5849d86da53868e
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-11-28 18:19:39 -0200

    add types and improve returns in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,113 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
-import { IGetNewAddressParams, IConfig } from './interfaces'
+import {
+    IConfig,
+    IGetTransactionInformationData,
+    IGetNewAddressData,
+    IGetNewAddressResponse,
+    IGetTransactionsResponse,
+    IGetTransactionsBody,
+    ITransaction,
+    IGetTransactionInformationResponse,
+} from './interfaces'
 import { ENDPOINT } from './constants'
 
-let token: string = ''
+let authorizationToken: string = ''
 
-const api = (token: string) =>
-    axios.create({
-        baseURL: 'https://api.warpexchange.com',
-        timeout: 15000,
-        headers: {
-            'Content-Type': 'application/json',
-            authorizationToken: token,
-        },
-    })
+const api = axios.create({
+    baseURL: 'https://api.warpexchange.com',
+    timeout: 15000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+
+api.interceptors.request.use(
+    function(config) {
+        config.headers['authorizationToken'] = authorizationToken
+
+        return config
+    },
+    function(err) {
+        return Promise.reject(err)
+    },
+)
 
 const convertSatoshiToBTC = (value: number) => value / 100000000
 
-const getNewAddress = async (data: IGetNewAddressParams) => {
+const getTransaction = (t: IGetTransactionsBody): ITransaction => ({
+    typeCurrency: t.TipoMoeda,
+    merchantSystemId: t.MerchantSystemID,
+    customerAddress: t.EnderecoDoCliente,
+    dateReceive: t.DataRecebimento,
+    valueRequiredInCurrencyLocal: t.ValorSolicitadoEmMoedaLocal,
+    notified: t.Notificado,
+    miningFeeSubmitted: t.TarifaMineracaoEnviada,
+    hashTransaction: t.HashDaTransacao,
+    ValueReceivedInDigital: t.ValorRecebidoEmDigital,
+    ValueRequestInDigital: t.ValorSolicitadoEmDigital,
+    statusId: t.StatusID,
+    status: t.Status,
+})
+
+const getNewAddress = async (
+    data: IGetNewAddressData,
+): Promise<AxiosResponse<IGetNewAddressResponse>> => {
     try {
-        return api(token).post(ENDPOINT.GETNEWADDRESS, data)
+        return api.post(ENDPOINT.GETNEWADDRESS, data)
     } catch (err) {
         throw err
     }
 }
 
-const warpExchange = ({ token }: IConfig) => {
-    token = token
+const getTransactionInformation = async (
+    data: IGetTransactionInformationData,
+): Promise<AxiosResponse<IGetTransactionInformationResponse>> => {
+    try {
+        const response = await api.post(ENDPOINT.GETNEWADDRESS, data)
+
+        return {
+            ...response,
+            data: {
+                ...response.data,
+                body: getTransaction(response.data.body),
+            },
+        }
+    } catch (err) {
+        throw err
+    }
+}
+
+const getTransactions = async (): Promise<
+    AxiosResponse<IGetTransactionsResponse>
+> => {
+    try {
+        const response = await api.post(ENDPOINT.GETTRANSACTIONS)
+
+        return {
+            ...response,
+            data: {
+                ...response.data,
+                body: response.data.body.map((t: IGetTransactionsBody) =>
+                    getTransaction(t),
+                ),
+            },
+        }
+    } catch (err) {
+        throw err
+    }
+}
+
+const WarpExchange = ({ token }: IConfig) => {
+    authorizationToken = token
 
     return {
         convertSatoshiToBTC,
         getNewAddress,
+        getTransactionInformation,
+        getTransactions,
     }
 }
 
-export default warpExchange
+module.exports = WarpExchange
+export default WarpExchange
