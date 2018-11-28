b7ebfc4116b0ffd6374bb136a29614c7a1506c52
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-11-28 17:10:48 -0200

    add IGetTransactionInformationBody and IGetTransactionInformationResponse

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -16,6 +16,35 @@ export interface IGetNewAddressData {
     split?: ISplit
 }
 
+export interface IGetNewAddressBody {
+    walletAddress: string
+    valueInSatoshi: number
+}
+
+export interface IGetNewAddressResponse {
+    statusCode: number
+    body: IGetNewAddressBody
+}
+
 export interface IGetTransactionInformationData {
     merchantSystemID: string
+}
+export interface IGetTransactionInformationBody {
+    TipoMoeda: string
+    MerchantSystemID: string
+    EnderecoDoCliente: null
+    DataRecebimento: null
+    ValorSolicitadoEmMoedaLocal: string
+    Notificado: boolean
+    TarifaMineracaoEnviada: null
+    HashDaTransacao: null
+    ValorRecebidoEmDigital: null
+    ValorSolicitadoEmDigital: string
+    StatusID: number
+    Status: string
+}
+
+export interface IGetTransactionInformationResponse {
+    statusCode: number
+    body: IGetTransactionInformationBody[]
 }
